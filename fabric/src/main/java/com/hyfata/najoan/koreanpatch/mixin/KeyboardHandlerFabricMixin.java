eedd5e0ac99c5700c5abf20848da92a65d58f7c7
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-09-19 06:39:49 +0900

    Add keyboard handler mixin for neoforge

--- fabric/src/main/java/com/hyfata/najoan/koreanpatch/mixin/KeyboardHandlerFabricMixin.java
+++ fabric/src/main/java/com/hyfata/najoan/koreanpatch/mixin/KeyboardHandlerFabricMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardHandler.class)
-public class KeyboardHandlerMixin {
+public class KeyboardHandlerFabricMixin {
     @Shadow
     @Final
     private Minecraft minecraft;
