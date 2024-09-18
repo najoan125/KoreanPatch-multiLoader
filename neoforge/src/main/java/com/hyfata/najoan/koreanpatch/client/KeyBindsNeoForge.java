d2bac511005a2032496c63a5fb6d7c1aa58b7b48
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-09-19 06:13:28 +0900

    Fix key binding for neofoege

--- neoforge/src/main/java/com/hyfata/najoan/koreanpatch/client/KeyBindsNeoForge.java
+++ neoforge/src/main/java/com/hyfata/najoan/koreanpatch/client/KeyBindsNeoForge.java
@@ -3,11 +3,10 @@ package com.hyfata.najoan.koreanpatch.client;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.sun.jna.Platform;
 import net.minecraft.client.KeyMapping;
-import net.neoforged.jarjar.nio.util.Lazy;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindsNeoForge {
-    private static Lazy<KeyMapping> langBinding, imeBinding;
+    private static KeyMapping langBinding, imeBinding;
 
     public static void register() {
         int keycode;
@@ -17,26 +16,26 @@ public class KeyBindsNeoForge {
             keycode = GLFW.GLFW_KEY_LEFT_CONTROL;
         }
 
-        langBinding = Lazy.of(() -> new KeyMapping(
+        langBinding = new KeyMapping(
                 "key.koreanpatch.toggle_langtype",
                 InputConstants.Type.KEYSYM,
                 keycode,
                 "key.categories.koreanpatch"
-        ));
+        );
 
-        imeBinding = Lazy.of(() -> new KeyMapping(
+        imeBinding = new KeyMapping(
                 "key.koreanpatch.toggle_ime",
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_I,
                 "key.categories.koreanpatch"
-        ));
+        );
     }
 
     public static KeyMapping getLangBinding() {
-        return langBinding.get();
+        return langBinding;
     }
 
     public static KeyMapping getImeBinding() {
-        return imeBinding.get();
+        return imeBinding;
     }
 }
