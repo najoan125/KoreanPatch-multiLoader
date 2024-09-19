077605cfb01d66c29d628d566b86ab8fe7845b5c
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-09-19 15:32:40 +0900

    chore

--- neoforge/src/main/java/com/hyfata/najoan/koreanpatch/client/KoreanPatchNeoForge.java
+++ neoforge/src/main/java/com/hyfata/najoan/koreanpatch/client/KoreanPatchNeoForge.java
@@ -11,12 +11,6 @@ import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 public class KoreanPatchNeoForge {
 
     public KoreanPatchNeoForge(IEventBus bus, ModContainer container) {
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Constants.LOG.info("Hello NeoForge world!");
         KoreanPatchClient.init();
 
         KeyBindsNeoForge.register();
