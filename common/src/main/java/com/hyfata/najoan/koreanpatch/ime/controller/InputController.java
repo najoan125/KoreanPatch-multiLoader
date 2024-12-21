54fb1152956e4d3d8901f83736f88aa522d077f9
Najoan <68422289+najoan125@users.noreply.github.com>
2024-12-21 15:53:46 +0900

    clean up

--- common/src/main/java/com/hyfata/najoan/koreanpatch/ime/controller/InputController.java
+++ common/src/main/java/com/hyfata/najoan/koreanpatch/ime/controller/InputController.java
@@ -2,7 +2,6 @@ package com.hyfata.najoan.koreanpatch.ime.controller;
 
 import com.hyfata.najoan.koreanpatch.ime.arch.win.WinController;
 import com.sun.jna.Platform;
-import org.lwjgl.glfw.GLFW;
 
 public interface InputController {
     void setFocus(final boolean focus);
