7df585ef4f31ebb7755d3b180fb46464a01ad3d6
Najoan <68422289+najoan125@users.noreply.github.com>
2025-05-04 20:58:57 +0900

    Refactor field order in CategoryInput class.
    Rearranged the fields in the `CategoryInput` class for better readability and logical grouping. This change improves code organization without altering functionality.

--- common/src/main/java/com/hyfata/najoan/koreanpatch/data/config/category/CategoryInput.java
+++ common/src/main/java/com/hyfata/najoan/koreanpatch/data/config/category/CategoryInput.java
@@ -6,9 +6,9 @@ import com.hyfata.najoan.koreanpatch.data.gson.JsonComment;
 public class CategoryInput {
     @JsonComment(value = "Auto language type mode: ", enums = true)
     private AutoLangTypeMode autoLangTypeMode = AutoLangTypeMode.AUTO;
+    private boolean memoryLangTypePerScreen = false;
     private boolean disableImeWhenPlaying = true;
     private boolean autoImeSwitch = true;
-    private boolean memoryLangTypePerScreen = false;
     private boolean alwaysImeEnabled = false;
 
     public AutoLangTypeMode getAutoLangTypeMode() {
