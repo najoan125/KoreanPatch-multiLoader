fc389eec241656e107b218a7ab83bbcefbdef806
najoan125 <tiaaa0012@gmail.com>
2025-08-12 12:13:31 +0900

    Remove `BookScreenVar` class as it is no longer in use.

--- common/src/main/java/com/hyfata/najoan/koreanpatch/helper/BookScreenVar.java
+++
@@ -1,7 +0,0 @@
-package com.hyfata.najoan.koreanpatch.helper;
-
-import com.hyfata.najoan.koreanpatch.indicator.AnimationHandler;
-
-public class BookScreenVar {
-    public static AnimationHandler animationHandler;
-}
