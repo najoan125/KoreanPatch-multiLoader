5efba0c8a928bd13b48d9dadfbfe92fa915ee222
najoan125 <tiaaa0012@gmail.com>
2025-10-14 09:53:14 +0900

    Update `FormattableEditBoxMixin`

--- fabric/src/main/java/com/hyfata/najoan/koreanpatch/mixin/mods/easy_anvils/FormattableEditBoxMixin.java
+++ fabric/src/main/java/com/hyfata/najoan/koreanpatch/mixin/mods/easy_anvils/FormattableEditBoxMixin.java
@@ -1,17 +1,13 @@
 package com.hyfata.najoan.koreanpatch.mixin.mods.easy_anvils;
 
-import com.hyfata.najoan.koreanpatch.client.GUIStatus;
 import com.hyfata.najoan.koreanpatch.wrapper.WrapperEditBox;
 import com.hyfata.najoan.koreanpatch.mixin.accessor.EditBoxAccessor;
-import com.hyfata.najoan.koreanpatch.process.LangTypeManager;
-import fuzs.easyanvils.client.gui.components.AdvancedEditBox;
 import fuzs.easyanvils.client.gui.components.FormattableEditBox;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.input.CharacterEvent;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.network.chat.Component;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,25 +15,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = {FormattableEditBox.class})
-public abstract class FormattableEditBoxMixin extends AdvancedEditBox {
+public abstract class FormattableEditBoxMixin extends EditBox {
     public FormattableEditBoxMixin(Font font, int x, int y, int width, int height, Component message) {
         super(font, x, y, width, height, message);
     }
 
     @Unique
-    private final Minecraft koreanPatch$client = Minecraft.getInstance();
+    private final WrapperEditBox koreanPatch$wrapper = new WrapperEditBox((EditBoxAccessor) this);
+
+    @Inject(at = {@At(value = "HEAD")}, method = {"charTyped"}, cancellable = true)
+    public void charTyped(CharacterEvent e, CallbackInfoReturnable<Boolean> cir) {
+        koreanPatch$wrapper.charTyped(e, cir, isEditable());
+    }
 
     @Unique
-    private final WrapperEditBox koreanPatch$handler = new WrapperEditBox((EditBoxAccessor) this);
-
-//    @Inject(at = {@At(value = "HEAD")}, method = {"charTyped(CI)Z"}, cancellable = true)
-//    public void charTyped(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-//        koreanPatch$handler.charTyped(chr, modifiers, cir);
-//    }
-//
-//    @Unique
-//    @Override
-//    public boolean keyPressed(KeyEvent keyEvent) {
-//        return koreanPatch$handler.keyPressed(keyEvent, () -> super.keyPressed(keyEvent));
-//    }
+    @Override
+    public boolean keyPressed(KeyEvent keyEvent) {
+        return koreanPatch$wrapper.keyPressed(keyEvent, () -> super.keyPressed(keyEvent));
+    }
 }
--- neoforge/src/main/java/com/hyfata/najoan/koreanpatch/mixin/mods/easy_anvils/FormattableEditBoxMixin.java
+++ neoforge/src/main/java/com/hyfata/najoan/koreanpatch/mixin/mods/easy_anvils/FormattableEditBoxMixin.java
@@ -1,15 +1,14 @@
 package com.hyfata.najoan.koreanpatch.mixin.mods.easy_anvils;
 
-import com.hyfata.najoan.koreanpatch.client.GUIStatus;
 import com.hyfata.najoan.koreanpatch.wrapper.WrapperEditBox;
 import com.hyfata.najoan.koreanpatch.mixin.accessor.EditBoxAccessor;
-import com.hyfata.najoan.koreanpatch.process.LangTypeManager;
-import fuzs.easyanvils.client.gui.components.AdvancedEditBox;
 import fuzs.easyanvils.client.gui.components.FormattableEditBox;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.input.CharacterEvent;
+import net.minecraft.client.input.KeyEvent;
 import net.minecraft.network.chat.Component;
-import org.lwjgl.glfw.GLFW;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,36 +16,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = {FormattableEditBox.class})
-public abstract class FormattableEditBoxMixin extends AdvancedEditBox {
+public abstract class FormattableEditBoxMixin extends EditBox {
     public FormattableEditBoxMixin(Font font, int x, int y, int width, int height, Component message) {
         super(font, x, y, width, height, message);
     }
 
-//    @Unique
-//    private final Minecraft _$client = Minecraft.getInstance();
-//
-//    @Unique
-//    private final WrapperEditBox _$handler = new WrapperEditBox((EditBoxAccessor) this);
-//
-//    @Inject(at = {@At(value = "HEAD")}, method = {"charTyped(CI)Z"}, cancellable = true)
-//    public void charTyped(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-//        if (this._$client.screen != null && !GUIStatus.getInstance().isBypassInjection() &&
-//                LangTypeManager.getInstance().isKorean() && this.isEditable && Character.charCount(chr) == 1) {
-//            _$handler.charTyped(chr, modifiers, cir);
-//        }
-//    }
-//
-//    @Unique
-//    @Override
-//    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-//        Minecraft client = Minecraft.getInstance();
-//        if (client.screen != null && !GUIStatus.getInstance().isBypassInjection()) {
-//            if (keyCode == GLFW.GLFW_KEY_BACKSPACE) {
-//                if (_$handler.onBackspaceKeyPressed()) {
-//                    return true;
-//                }
-//            }
-//        }
-//        return super.keyPressed(keyCode, scanCode, modifiers);
-//    }
+    @Unique
+    private final WrapperEditBox koreanPatch$wrapper = new WrapperEditBox((EditBoxAccessor) this);
+
+    @Inject(at = {@At(value = "HEAD")}, method = {"charTyped"}, cancellable = true)
+    public void charTyped(CharacterEvent e, CallbackInfoReturnable<Boolean> cir) {
+        koreanPatch$wrapper.charTyped(e, cir, isEditable);
+    }
+
+    @Unique
+    @Override
+    public boolean keyPressed(@NotNull KeyEvent keyEvent) {
+        return koreanPatch$wrapper.keyPressed(keyEvent, () -> super.keyPressed(keyEvent));
+    }
 }
