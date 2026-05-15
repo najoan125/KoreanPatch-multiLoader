ff29b5989b98e1a9b93f39b90e5eaa503e69a8f6
Najoan <tiaaa0012@gmail.com>
2026-05-15 06:07:32 +0000

    Add: cocoainput load test

---
+++ common/src/test/java/driver/CocoainputLoadTest.java
@@ -0,0 +1,11 @@
+package driver;
+import org.junit.jupiter.api.Test;
+
+import com.hyfata.najoan.koreanpatch.driver.InputController;
+
+public class CocoainputLoadTest {
+    @Test
+    void newInputController() {
+        InputController.newController();
+    }
+}
