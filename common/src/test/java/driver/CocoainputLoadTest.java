16c2f15052bdf5b713d5011221282470b9ff7df6
Najoan <tiaaa0012@gmail.com>
2026-05-15 07:51:37 +0000

    Fucking: VS Code is so shit IDE and not working gradle, java extensions. It is the worst in my life. Notepad++ is much better.

--- common/src/test/java/driver/CocoainputLoadTest.java
+++ common/src/test/java/driver/CocoainputLoadTest.java
@@ -2,6 +2,7 @@ package driver;
 import org.junit.jupiter.api.Test;
 
 import com.hyfata.najoan.koreanpatch.driver.InputController;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CocoainputLoadTest {
     @Test
